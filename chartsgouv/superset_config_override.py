729da5f4750e854a71313850742725d34eb0ea01
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-05-23 13:30:56 +0000

    feat: rename files in import

--- chartsgouv/superset_config_override.py
+++ chartsgouv/superset_config_override.py
@@ -1,11 +1,11 @@
-from custom_config.superset_feature_flags import SECRET_KEY, FEATURE_FLAGS
-from custom_config.superset_talisman import TALISMAN_ENABLED, TALISMAN_CONFIG
-from custom_config.superset_theme import (
+from custom_config.feature_flags import SECRET_KEY, FEATURE_FLAGS
+from custom_config.talisman import TALISMAN_ENABLED, TALISMAN_CONFIG
+from custom_config.theme import (
     THEME_OVERRIDES,
     EXTRA_CATEGORICAL_COLOR_SCHEMES,
     EXTRA_SEQUENTIAL_COLOR_SCHEMES
 )
-from custom_config.superset_cache_config import (
+from custom_config.cache_config import (
     CACHE_DEFAULT_TIMEOUT,
     CACHE_CONFIG,
     DATA_CACHE_CONFIG,
