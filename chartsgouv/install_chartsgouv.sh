5e03e121709ec9658616d4e3324309e61af688c2
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-05-26 08:51:12 +0000

    fix: update file paths

--- chartsgouv/install_chartsgouv.sh
+++ chartsgouv/install_chartsgouv.sh
@@ -1,13 +1,13 @@
 kubectl delete configmap superset-tmp-custom-extra-config
 kubectl create configmap superset-tmp-custom-extra-config \
-    --from-file=custom_config/superset_cache_config.py \
-    --from-file=custom_config/superset_custom_security_manager.py \
-    --from-file=custom_config/superset_custom_user_model.py \
-    --from-file=custom_config/superset_feature_flags.py \
-    --from-file=custom_config/superset_html_sanitization.py \
-    --from-file=custom_config/superset_jinja_context_addons.py \
-    --from-file=custom_config/superset_talisman.py \
-    --from-file=custom_config/superset_theme.py
+    --from-file=custom_config/cache_config.py \
+    --from-file=custom_config/security_manager.py \
+    --from-file=custom_config/user_model.py \
+    --from-file=custom_config/feature_flags.py \
+    --from-file=custom_config/html_sanitization.py \
+    --from-file=custom_config/jinja_context_addons.py \
+    --from-file=custom_config/talisman.py \
+    --from-file=custom_config/theme.py
 
 helm repo add superset http://apache.github.io/superset/
 helm repo update superset
