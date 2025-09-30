f2f7507cfa775da5089c76f18ade613e9b26b302
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-09-30 15:42:44 +0000

    feat(ansible): add base for chartsgouv playbook

--- ansible/init.bash
+++ ansible/init.bash
@@ -4,5 +4,9 @@
 echo Installing Ansible on the control node ...
 python3 -m pip install --user ansible
 
+echo "Début: $(date)"
 # App deployment
-ansible-playbook -i localhost playbooks/airflow.yaml
+# ansible-playbook -i localhost playbooks/airflow.yaml
+ansible-playbook -i localhost playbooks/postgresql.yaml
+
+echo "Fin: $(date)"
