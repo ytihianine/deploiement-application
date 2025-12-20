2c8fbe9a205e3ffd02a3c21e3900166130b822ca
ytihianine <yanis.tihianine@finances.gouv.fr>
2025-12-20 16:34:30 +0000

    feat: add generic method to generates files to configure

--- ansible/init.bash
+++ ansible/init.bash
@@ -10,6 +10,16 @@ pb_postgresql=playbooks/postgresql.yaml
 echo "Installing Ansible on the control node ..."
 python3 -m pip install --user ansible
 
+# Function to duplicate
+duplicate_files() {
+    echo "Duplicating files ..."
+    find . -type f -name "example.main.yaml" -exec sh -c '
+        target="$(dirname "$1")/main.yaml"
+        cp "$1" "$target"
+        echo "Created: $target"
+    ' _ {} \;
+}
+
 # Function to deploy a playbook
 deploy_playbook() {
     local pb="$1"
@@ -19,17 +29,21 @@ deploy_playbook() {
 
 # Menu
 echo "Select the apps to deploy:"
+echo "0) Dupliquer fichiers d'exemple"
 echo "1) Airflow"
 echo "2) PostgreSQL"
 echo "3) Chartsgouv"
 echo "4) All"
 echo "5) Exit"
 
-read -p "Enter your choice (1-5): " choice
+read -p "Enter your choice (0-5): " choice
 
 echo "Début: $(date)"
 
 case "$choice" in
+    0)
+        duplicate_files
+        ;;
     1)
         deploy_playbook "$pb_airflow"
         ;;
