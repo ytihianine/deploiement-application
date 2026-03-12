848dccee30dd28d986a844671597d3e32b332802
ytihianine <yanis.tihianine@finances.gouv.fr>
2026-03-12 10:12:42 +0000

    feat(ansible/chartsgouv): update superset config to match v6 version

--- ansible/roles/apps/chartsgouv/files/superset_config_override.py
+++ ansible/roles/apps/chartsgouv/files/superset_config_override.py
@@ -2,16 +2,6 @@
 from typing import Any
 
 # ------------------------
-# Application
-# ------------------------
-# Specify the App icon
-APP_NAME = "ChartsGouv"
-APP_ICON = "/static/assets/local/images/app_icon_avec_titre_horizontal.png"
-LOGO_TOOLTIP = "ChartsGouv"
-
-FAVICONS = [{"href": "/static/assets/dsfr/favicon/favicon.svg"}]
-
-# ------------------------
 # Application - configuration
 # ------------------------
 SUPERSET_DASHBOARD_POSITION_DATA_LIMIT = 6553500
@@ -181,6 +171,235 @@ FEATURE_FLAGS: dict[str, bool] = {
 # Pour plus d'informations: https://github.com/apache/superset/blob/886f52554539318521858fbcf493123c8c4199ef/superset/config.py#L940
 HTML_SANITIZATION = False
 HTML_SANITIZATION_SCHEMA_EXTENSIONS: dict[str, Any] = {}
+
+# ------------------------
+# Thèmes
+# ------------------------
+THEME_DEFAULT = {
+  "token": {
+    "wireframe": False,
+    "brandAppName": "ChartsGouv",  # Window titles
+    "brandLogoAlt": "ChartsGouv",  # Logo alt text
+    "brandLogoUrl": "/static/assets/local/images/app_icon_avec_titre_horizontal.png",
+    "colorPrimary": "#000091",
+    "colorInfo": "#000091",
+    "colorError": "#ce0500",
+    "colorSuccess": "#18753c",
+    "colorWarning": "#b34000",
+    "colorPrimaryHover": "#1212ff",
+    "colorPrimaryActive": "#2323ff",
+    "colorSuccessHover": "#27a959",
+    "colorSuccessActive": "#2fc368",
+    "colorWarningHover": "#ff6218",
+    "colorWarningActive": "#ff7a55",
+    "colorErrorHover": "#ff2725",
+    "colorErrorActive": "#ff4140",
+    "colorLink": "#000091",
+    "colorLinkHover": "#1212ff",
+    "colorLinkActive": "#2323ff",
+    "colorSuccessText": "#18753c",
+    "colorWarningText": "#ffffff",
+    "colorWarningTextActive": "#ffffff",
+    "colorWarningTextHover": "#ffffff",
+    "colorErrorTextHover": "#ffffff",
+    "colorErrorText": "#ffffff",
+    "colorErrorTextActive": "#ffffff",
+    "colorInfoTextHover": "#ffffff",
+    "colorInfoText": "#ffffff",
+    "colorInfoTextActive": "#ffffff",
+    "colorInfoHover": "#98b4ff",
+    "colorInfoActive": "#b4c7ff",
+    "colorTextBase": "#3a3a3a"
+  },
+  "components": {
+    "Button": {
+      "borderRadius": 0,
+      "borderRadiusSM": 0,
+      "borderRadiusLG": 0,
+      "textTextColor": "rgb(255,255,255)",
+      "textTextActiveColor": "rgb(255,255,255)",
+      "textTextHoverColor": "rgb(255,255,255)",
+      "solidTextColor": "rgb(255,255,255)",
+      "colorText": "rgb(255,255,255)",
+      "colorPrimaryText": "rgb(106,106,244)",
+      "colorPrimaryTextActive": "rgb(255,255,255)",
+      "colorPrimaryTextHover": "rgb(255,255,255)",
+      "colorBgTextActive": "rgb(255,255,255)",
+      "textHoverBg": "rgb(255,255,255)",
+      "colorBgContainerDisabled": "rgb(255,255,255)",
+      "colorBgSolid": "rgb(255,255,255)",
+      "colorBgSolidActive": "rgb(255,255,255)",
+      "colorBgSolidHover": "rgb(255,255,255)",
+      "colorPrimaryBgHover": "rgb(18,18,255)",
+      "colorTextLightSolid": "rgb(255,255,255)",
+      "defaultColor": "rgb(0,0,145)",
+      "algorithm": True,
+      "colorError": "rgb(201,25,30)",
+      "colorErrorActive": "rgb(249,90,92)",
+      "colorErrorHover": "rgb(249,63,66)",
+      "primaryShadow": "",
+      "defaultBg": "rgb(238,238,238)",
+      "defaultBorderColor": "rgb(0,0,145)"
+    },
+    "Tag": {
+      "colorSuccessText": "#ffffff",
+      "borderRadiusSM": 0
+    },
+    "Typography": {
+      "fontFamilyCode": "'Marianne','SFMono-Regular', Consolas, 'Liberation Mono', Menlo, Courier, monospace",
+      "colorText": "rgb(58,58,58)",
+      "algorithm": True,
+      "fontSize": 16,
+      "fontSizeHeading1": 40,
+      "fontSizeHeading2": 32,
+      "fontSizeHeading3": 28,
+      "fontSizeHeading4": 24,
+      "fontSizeHeading5": 22
+    },
+    "InputNumber": {
+      "activeBg": "rgb(238,238,238)",
+      "activeBorderColor": "rgb(0,0,145)",
+      "addonBg": "rgb(238,238,238)",
+      "handleBorderColor": "rgb(238,238,238)",
+      "handleHoverColor": "rgb(0,0,145)",
+      "hoverBorderColor": "rgb(0,0,145)",
+      "colorBgContainer": "rgb(238,238,238)",
+      "borderRadius": 0,
+      "borderRadiusLG": 0,
+      "borderRadiusSM": 0,
+      "hoverBg": "rgb(238,238,238)",
+      "activeShadow": "",
+      "errorActiveShadow": "",
+      "warningActiveShadow": "",
+      "motionDurationMid": "0s",
+      "motionDurationSlow": "0s",
+      "colorText": "rgb(58,58,58)"
+    },
+    "Select": {
+      "motionDurationSlow": "0.s",
+      "motionDurationMid": "0.s",
+      "borderRadius": 0,
+      "borderRadiusLG": 0,
+      "borderRadiusSM": 0,
+      "borderRadiusXS": 0,
+      "activeBorderColor": "rgb(0,0,145)",
+      "boxShadowSecondary": "",
+      "activeOutlineColor": "rgb(255,255,255)",
+      "hoverBorderColor": "rgb(0,0,145)",
+      "optionSelectedBg": "rgb(238,238,238)",
+      "colorBgBase": "rgb(238,238,238)"
+    },
+    "Dropdown": {
+      "boxShadowPopoverArrow": "",
+      "motionDurationMid": "0s",
+      "boxShadowSecondary": "",
+      "borderRadiusLG": 0,
+      "borderRadiusSM": 0,
+      "borderRadiusXS": 0
+    },
+    "Menu": {
+      "motionDurationSlow": "0s",
+      "motionDurationMid": "0s",
+      "motionDurationFast": "0s",
+      "boxShadowSecondary": "",
+      "borderRadius": 0,
+      "borderRadiusLG": 0,
+      "itemBorderRadius": 0,
+      "subMenuItemBorderRadius": 0,
+      "itemHoverBg": "rgb(238,238,238)",
+      "subMenuItemBg": "rgb(255,255,255)",
+      "subMenuItemSelectedColor": "rgb(0,0,145)",
+      "itemColor": "rgb(58,58,58)",
+      "colorText": "rgb(58,58,58)",
+      "horizontalItemSelectedBg": "rgb(227,227,253)",
+      "horizontalItemSelectedColor": "rgb(0,0,145)"
+    },
+    "Slider": {
+      "railSize": 8,
+      "handleSize": 16,
+      "borderRadiusXS": 20,
+      "railBg": "rgb(255,255,255)",
+      "railHoverBg": "rgb(255,255,255)",
+      "trackBg": "rgb(0,0,145)",
+      "colorPrimaryBorderHover": "rgb(0,0,145)",
+      "handleActiveColor": "rgb(0,0,145)",
+      "handleColor": "rgb(0,0,145)",
+      "handleLineWidth": 1,
+      "handleLineWidthHover": 1,
+      "handleSizeHover": 16
+    },
+    "Pagination": {
+      "borderRadius": 0,
+      "borderRadiusLG": 0,
+      "borderRadiusSM": 0,
+      "itemInputBg": "rgb(238,238,238)",
+      "colorBgContainer": "rgba(255,255,255,0)",
+      "controlHeight": 32,
+      "colorBgTextActive": "rgb(255,255,255)",
+      "colorPrimaryBorder": "rgb(0,0,145)",
+      "colorPrimaryHover": "rgb(0,0,145)",
+      "colorBgTextHover": "rgb(58,58,58)"
+    },
+    "Input": {
+      "borderRadius": 0,
+      "borderRadiusLG": 0,
+      "borderRadiusSM": 0,
+      "activeBg": "rgb(238,238,238)",
+      "activeBorderColor": "rgb(0,0,145)",
+      "hoverBorderColor": "rgb(0,0,145)",
+      "hoverBg": "rgb(238,238,238)",
+      "addonBg": "rgb(238,238,238)",
+      "colorText": "rgb(58,58,58)"
+    },
+    "Modal": {
+      "borderRadiusLG": 0,
+      "borderRadiusSM": 0,
+      "colorText": "rgb(22,22,22)",
+      "titleFontSize": 20,
+      "colorIcon": "rgb(0,0,145)",
+      "colorIconHover": "rgb(0,0,145)",
+      "colorBgTextHover": "rgb(238,238,238)"
+    },
+    "Tooltip": {
+      "borderRadius": 0,
+      "borderRadiusXS": 0,
+      "colorTextLightSolid": "rgb(58,58,58)",
+      "colorBgSpotlight": "rgb(255,255,255)"
+    },
+    "Alert": {
+      "borderRadiusLG": 0
+    },
+    "Message": {
+      "borderRadiusLG": 0
+    },
+    "Popover": {
+      "borderRadiusLG": 0,
+      "borderRadiusXS": 0,
+      "fontWeightStrong": 800
+    },
+    "Checkbox": {
+      "borderRadiusSM": 2,
+      "controlInteractiveSize": 18,
+      "colorPrimaryHover": "rgb(0,0,145)"
+    },
+    "Tabs": {
+      "borderRadius": 0,
+      "borderRadiusLG": 0,
+      "inkBarColor": "rgb(0,0,145)",
+      "itemActiveColor": "rgb(0,0,145)",
+      "itemHoverColor": "rgba(58,58,58,0.88)",
+      "cardBg": "rgb(227,227,253)"
+    },
+    "Table": {
+      "headerBg": "rgb(246,246,246)",
+      "borderRadius": 0,
+      "headerBorderRadius": 0
+    }
+  }
+}
+
+THEME_DARK = None
+
 
 # ------------------------
 # Couleurs & Palettes
@@ -360,88 +579,6 @@ DSFR_CHART_COLORS = {
 # https://preset.io/blog/theming-superset-progress-update/
 # [SIP-82] Improving Superset Theming https://github.com/apache/superset/issues/20159
 # https://www.systeme-de-design.gouv.fr/version-courante/fr/fondamentaux/couleurs--palette
-THEME_OVERRIDES = {
-    "borderRadius": 5,
-    "colors": {
-        "text": {
-            "label": DSFR_COLORS["sun"]["grey-425-625"],
-            "help": DSFR_COLORS["sun"]["grey-425-625"],
-        },
-        "primary": {
-            "base": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "dark1": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "dark2": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "light1": DSFR_COLORS["sun"]["blue-france-main-525"],
-            "light2": DSFR_COLORS["sun"]["blue-france-850-200"],
-            "light3": DSFR_COLORS["sun"]["blue-france-925-125"],
-            "light4": DSFR_COLORS["sun"]["blue-france-950-100"],
-            "light5": DSFR_COLORS["sun"]["blue-france-975-75"],
-        },
-        "secondary": {
-            "base": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "dark1": DSFR_COLORS["sun"]["grey-0-1000"],
-            "dark2": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "dark3": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "light1": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "light2": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "light3": DSFR_COLORS["sun"]["blue-france-sun-113-625"],
-            "light4": DSFR_COLORS["sun"]["blue-france-925-125"],
-            "light5": DSFR_COLORS["sun"]["blue-france-925-125"],
-        },
-        "grayscale": {
-            "base": DSFR_COLORS["sun"]["grey-425-625"],
-            "dark1": DSFR_COLORS["sun"]["grey-200-850"],
-            "dark2": DSFR_COLORS["sun"]["grey-50-1000"],
-            "light1": DSFR_COLORS["sun"]["grey-625-425"],
-            "light2": DSFR_COLORS["sun"]["grey-925-125"],
-            "light3": DSFR_COLORS["sun"]["grey-950-100"],
-            "light4": DSFR_COLORS["sun"]["grey-975-75"],
-            "light5": DSFR_COLORS["sun"]["grey-1000-50"],
-        },
-        "error": {
-            "base": DSFR_COLORS["sun"]["error-425-625"],
-            "dark1": DSFR_COLORS["sun"]["error-425-625"],
-            "dark2": DSFR_COLORS["sun"]["grey-0-1000"],
-            "light1": DSFR_COLORS["sun"]["error-425-625"],
-            "light2": DSFR_COLORS["sun"]["error-950-100"],
-        },
-        "warning": {
-            "base": DSFR_COLORS["sun"]["warning-425-625"],
-            "dark1": DSFR_COLORS["sun"]["warning-425-625"],
-            "dark2": DSFR_COLORS["sun"]["grey-0-1000"],
-            "light1": DSFR_COLORS["sun"]["warning-425-625"],
-            "light2": DSFR_COLORS["sun"]["warning-950-100"],
-        },
-        "alert": {
-            "base": DSFR_COLORS["sun"]["green-tilleul-verveine-925-125"],
-            "dark1": DSFR_COLORS["sun"]["green-tilleul-verveine-925-125"],
-            "dark2": DSFR_COLORS["sun"]["grey-0-1000"],
-            "light1": DSFR_COLORS["sun"]["green-tilleul-verveine-925-125"],
-            "light2": DSFR_COLORS["sun"]["green-tilleul-verveine-975-75"],
-        },
-        "success": {
-            "base": DSFR_COLORS["sun"]["success-425-625"],
-            "dark1": DSFR_COLORS["sun"]["success-425-625"],
-            "dark2": DSFR_COLORS["sun"]["grey-0-1000"],
-            "light1": DSFR_COLORS["sun"]["success-425-625"],
-            "light2": DSFR_COLORS["sun"]["success-950-100"],
-        },
-        "info": {
-            "base": DSFR_COLORS["sun"]["info-425-625"],
-            "dark1": DSFR_COLORS["sun"]["info-425-625"],
-            "dark2": DSFR_COLORS["sun"]["grey-0-1000"],
-            "light1": DSFR_COLORS["sun"]["info-425-625"],
-            "light2": DSFR_COLORS["sun"]["info-950-100"],
-        },
-    },
-    "typography": {
-        "families": {
-            "sansSerif": "Marianne, Inter, Helvetica, Arial",
-            "serif": "Marianne, Georgia, Times New Roman, Times, serif",
-            "monospace": "Marianne, Fira Code, Courier New, monospace",
-        },
-    },
-}
 
 # EXTRA_CATEGORICAL_COLOR_SCHEMES is used for adding custom categorical color schemes
 # see DSFR colors "Couleurs illustratives"
